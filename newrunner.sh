7768156772a67535cc5d30ef5f3a8748e7865aee
Isaac Hartford <pianista1337@gmail.com>
2025-09-20 21:11:36 +0800

    Updates. Already built/pushed container. Should be ready to merge back to main

--- newrunner.sh
+++ newrunner.sh
@@ -18,16 +18,26 @@ case "${WORKSPACE}" in
 esac
 log "WORKSPACE='${WORKSPACE:-<unset>}' -> backend='${BACKEND}'"
 
-xdbg -d -b "${BACKEND}" generate --entity identity --amount 10 --concurrency 1
-xdbg -d -b "${BACKEND}" generate --entity group --amount 1 --concurrency 1 --invite 1
 
 while true; do 
-  log "Identities..."
-  xdbg -d -b "${BACKEND}" generate --entity identity --amount 1 --concurrency 1
-  log "Groups..."
-  xdbg -d -b "${BACKEND}" generate --entity group --amount 1 --concurrency 1 --invite 1
-  log "Messages..."
-  xdbg -d -b "${BACKEND}" generate --entity message --amount 1 --concurrency 1
-  log "Sleeping $XDBG_LOOP_PAUSE seconds..."
-  sleep $XDBG_LOOP_PAUSE
+  log "Reset environment.."
+  XDBG_LOOP_PAUSE=0 xdbg -d -b "${BACKEND}" --clear
+  XDBG_LOOP_PAUSE=0 xdbg -d -b "${BACKEND}" generate --entity identity --amount 5 --concurrency 1
+  XDBG_LOOP_PAUSE=0 xdbg -d -b "${BACKEND}" generate --entity group --amount 1 --concurrency 1 --invite 1
+  log "Reset complete, starting tests"
+  
+  for x in {1..10}; {
+    log "Identities..."
+    XDBG_LOOP_PAUSE=0 xdbg -d -b "${BACKEND}" generate --entity identity --amount 1 --concurrency 1
+    log "Sleeping 20s..."
+    sleep 20
+    log "Groups..."
+    XDBG_LOOP_PAUSE=0 xdbg -d -b "${BACKEND}" generate --entity group --amount 1 --concurrency 1 --invite 1
+    log "Sleeping 20s..."
+    sleep 20
+    log "Messages..."
+    XDBG_LOOP_PAUSE=0 xdbg -d -b "${BACKEND}" generate --entity message --amount 1 --concurrency 1
+    log "Sleeping $XDBG_LOOP_PAUSE seconds..."
+    sleep $XDBG_LOOP_PAUSE
+  }
 done
